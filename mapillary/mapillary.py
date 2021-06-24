b96985ca7c7ab38b37c2e965ec6084372ca5aa1c
Saif Ul Islam <saifulislam84210@gmail.com>
2021-06-24 07:07:41 +0500

    Initial code for the package

---
+++ mapillary/mapillary.py
@@ -0,0 +1,10 @@
+def greetings(name):
+    """
+    Print a line about converting a notebook.
+    Args:
+        my_name (str): person's name
+    Returns:
+        None
+    """
+
+    print(f"Hello, there, {name}! Nice to meet you. As you can see, work is currently under progress")
