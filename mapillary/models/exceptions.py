961dd39d71db027dc704d7e0843a96a08111080e
Saif Ul Islam <saifulislam84210@gmail.com>
2021-06-26 17:23:06 +0500

    feat: Exception handling for the project
    Created to make it easy to throw custom errors when some requirements are broken, and to make sense to the end user on how to fix it
    
    To be discussed in more detail with team

---
+++ mapillary/models/exceptions.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+"""
+mapillary.models.exceptions
+
+This module contains the set of Mapillary's exceptions
+"""
+
+
+class MapillaryException(Exception):
+    """Base class for exceptions in this module"""
+
+    pass
+
+
+class InvalidTokenError(MapillaryException):
+    """Raised when an invalid token is given
+    to access Mapillary's API, primarily used in mapillary.set_access_token
+
+    :var message: The error message returned
+    :var type: The type of error that occurred
+    :var code: The error code returned,
+    most like 190, "Access token has expired",
+    see
+    https://developers.facebook.com/docs/graph-api/using-graph-api/error-handling/
+    for more information
+    :fbtrace_id: A unique ID to track the issue/exception
+    """
+
+    def __init__(self, message, type, code, fbtrace_id):
+        """ """
+        self.message = message
+        self.type = type
+        self.code = code
+        self.fbtrace_id = fbtrace_id
+
+    def __str__(self):
+        return f'InvalidTokenError: An exception occured, "{self.message}"'
+
+    def __repr__(self):
+        return (
+            "InvalidTokenError: An exception occured."
+            + f'Message: "{self.message}", Type: "{self.type}",'
+            + f'Code: "{self.code}",'
+            + f'fbtrace_id: "{self.fbtrace_id}"'
+        )
