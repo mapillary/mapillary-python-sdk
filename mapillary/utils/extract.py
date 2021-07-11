af62670add4c67de26d534744f9b35fa8ddd36b0
Saif Ul Islam <saifulislam84210@gmail.com>
2021-07-11 14:18:45 +0500

    Added documentation, create test, for extract_properties

--- mapillary/utils/extract.py
+++ mapillary/utils/extract.py
@@ -7,7 +7,31 @@ nested within a GeoJSON packet
 """
 
 
-def extract_properties(geojson, properties):
+def extract_properties(geojson: dict, properties: list) -> dict:
+    """Extracts specific properties from a complete GeoJSON
+
+    :param geojson: GeoJSON object
+    :type geojson: dict
+
+    :param properties: A list of properties to extract
+    :type properties: list
+
+    :return: The extracted fields as a dict
+    :rtype: dict
+
+    Usage::
+        >>> from utils.extract import extract_properties
+        >>> extract_properties(data, ['id']) # id most likely exists
+
+    Import::
+        >>> cd ${MAPILLARY_PROJECT_ROOT}
+        >>> from tests.utils.test_extract import test_extract_properties
+        >>> test_extract_properties(['id'])
+        {"Test": "Success"}
+    
+    Test::
+        >>> pytest -m tests/    
+    """
 
     extracted_fields = {}
 
---
+++ tests/utils/test_extract.py
@@ -0,0 +1,29 @@
+import requests
+import mercantile
+from vt2geojson.tools import vt_bytes_to_geojson
+from mapillary.utils.extract import extract_properties
+
+def test_extract_properties(properties: list) -> dict:
+
+    tile = mercantile.tile(lng=31, lat=30, zoom=14)
+
+    data = vt_bytes_to_geojson(
+        b_content=requests.get(
+            f"https://tiles.mapillary.com/maps/vtp/mly1_public/2/14/{tile[0]}/{tile[1]}/?access_token=MLY|4352045404840373|b35c62c790e9b52c8476cfd08eb58704"
+        ).content,
+        x=tile.x,
+        y=tile.y,
+        z=tile.z,
+        layer="image",
+    )
+
+    extracted = extract_properties(data, properties)
+
+    assert('id' in extracted)
+
+    assert(len(extracted['id']) != 0)
+
+    return {"Test": "Success"}
+
+if __name__ == '__main__':
+    test_extract_properties(['id'])
