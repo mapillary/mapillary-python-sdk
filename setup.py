bf22f5acb58f10dbd7b5a31df50fbaf6602536b9
Saif Ul Islam <saifulislam84210@gmail.com>
2021-08-20 17:46:22 +0500

    Make first patch release
    Signed-off-by: Saif Ul Islam <saifulislam84210@gmail.com>

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ import shutil
 
 # Setup variables. Change as needed
 NAME = "mapillary"
-VERSION = "0.0.3"
+VERSION = "0.0.1"
 AUTHOR = "Christopher Beddow, Omar Ali, Saif Ul Islam"
 AUTHOR_EMAIL = "cbed@fb.com, omarmuhammed1998.om@gmail.com, saifulislam84210@gmail.com"
 LICENSE = "MIT"
